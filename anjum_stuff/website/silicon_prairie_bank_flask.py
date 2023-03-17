e82aea2fe2089bd9c9b395f3266692d358520679
sajid1760 <61717892+sajid1760@users.noreply.github.com>
2023-03-16 20:46:07 -0500

    Project-4-Group-2

---
+++ anjum_stuff/website/silicon_prairie_bank_flask.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, jsonify, send_from_directory, request
+import json
+import pandas as pd
+import numpy as np
+import os
+import pickle
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/data")
+def data():
+    return render_template("data.html")
+
+@app.route("/references")
+def references():
+    return render_template("references.html")
+
+@app.route("/credit_card")
+def credit_card():
+    # Return template and data
+    return render_template("credit_card.html")
+
+
+
+
+
+
+#####################################################################
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
+
+#main
+if __name__ == "__main__":
+    app.run(debug=True)
